ffbbda740a156d7997c97515acbe738fc089822c
Steven Barrett <barrett1733@sbcglobal.net>
2015-04-27 01:04:59 -0700

    Global variables because ugh monster.

--- config/Config.h
+++ config/Config.h
@@ -1,9 +1,16 @@
 #pragma once
-#include <map>
-#include "SFML\Graphics\Color.hpp"
 
-typedef std::map<std::string, sf::Color> ColorMap;
-static ColorMap colorConfig;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
 
-typedef std::map<std::string, std::string> StringMap;
-static StringMap stringConfig;
+const int controlPanelWidth = 200;
+const int gridDisplaySize = 600;
+
+const int buttonSize = 100;
+
+const sf::FloatRect controlPanelRect(SCREEN_WIDTH - controlPanelWidth, 0, controlPanelWidth, SCREEN_HEIGHT);
+const float controlPanelSize = (float)controlPanelWidth / SCREEN_WIDTH;
+
+const sf::FloatRect controlPanelViewport(1 - controlPanelSize, 0, controlPanelSize, 1);
+
+static int LEVELSIZE = 10;
