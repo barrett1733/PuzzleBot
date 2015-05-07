0694c6ce2310b4f7ce829490e5df05d7b27df3ee
Steven Barrett <barrett1733@sbcglobal.net>
2015-05-06 17:44:12 -0700

    Level manager can now generate an obstruction map from the entities.

--- manager/LevelManager.cpp
+++ manager/LevelManager.cpp
@@ -14,17 +14,18 @@ GridBool* LevelManager::getLevel(int num)
 	}
 }
 
-Position LevelManager::parseCoords(std::string coord)
+GridBool* LevelManager::getObsMap()
 {
-	char delimiter = ',';
-	int lineDelim = coord.find(delimiter);
-	std::string x, y;
-
-	x = coord.substr(0, lineDelim);
-	y = coord.substr(lineDelim + 1, coord.size());
-
-	return Position(atoi(x.c_str()), atoi(y.c_str()));
-
+	for (int y = 0; y < Config::LEVELSIZE; y++)
+		for (int x = 0; x < Config::LEVELSIZE; x++)
+			obsMap.at(x, y) = levels[0].at(x, y);
+	std::vector<Position> obsVec = entityManager->blockedPositions();
+	for (int i = 0; i < obsVec.size(); i++)
+	{
+		obsVec[i].round();
+		obsMap.at(obsVec[i]) = true;
+	}
+	return &obsMap;
 }
 
 void LevelManager::store(std::string name, std::string data)
@@ -37,6 +38,7 @@ void LevelManager::store(std::string name, std::string data)
 	{
 		Config::LEVELSIZE = atoi(data.c_str());
 		levels.push_back(GridBool(Config::LEVELSIZE, Config::LEVELSIZE));
+		obsMap.alloc(Config::LEVELSIZE, Config::LEVELSIZE);
 		levels[currentLevelLoading].clear(false);
 	}
 	else if (name == "wall")
--- manager/LevelManager.h
+++ manager/LevelManager.h
@@ -2,15 +2,19 @@
 #include <vector>
 #include "../utility/grid.h"
 #include "../load/LoadFile.h"
+#include "../load/AdditionalParsing.h"
+#include "EntityManager.h"
 
-class LevelManager : public LoadFile
+class LevelManager : public LoadFile, public AdditionalParsing
 {
 	std::vector<GridBool> levels;
-	Position parseCoords(std::string);
+	GridBool obsMap;
 	int currentLevelLoading, currentLevel;
 public:
 	LevelManager();
+	EntityManager* entityManager;
 	GridBool* getLevel(int num);
+	GridBool* getObsMap();
 	void store(std::string, std::string);
 };
 
